995c98be4b3bb50de44c06bed6439b68a8250ea4
owk4057 <owk4057@gmail.com>
2017-12-05 17:56:07 +0900

    Update bluetooth.h
    using dma

--- Libraries/bluetooth.h
+++ Libraries/bluetooth.h
@@ -1,28 +1,19 @@
-
-#ifndef __BLUETOOTH_H__
-#define __BLUETOOTH_H__
-#define USART_RX_BUFFER_SIZE 20
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
 
 #include "misc.h"
 #include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_usart.h"
-#include "stm32f10x_rcc.h"
+#include "stm32f10x_dma.h"
 
-typedef struct {
-	uint16_t BT_TX;
-	uint16_t BT_RX;
-	GPIO_TypeDef* BT_GPIO;
-	char BT_RX_Buffer[USART_RX_BUFFER_SIZE];
-	uint16_t BT_RX_Counter;
-} BT_Struct;
+#define BT_RX_BUFFER_SIZE (1000) 
 
-void BT_Struct_Init(void);
-void BT_RCC_init(void);
-void BT_init(void);
-void BT_IRQHandler(void);
-void USART2_IRQHandler(void);
+void Send_String_Usart2(int len);
+void BT_Struct_init(); 
+void BT_Rcc_init(); 
+void BT_Init(); 
+void USART2_IRQHandler(); 
 
-void BT_Run(void);
-
-#endif
+#endif 
