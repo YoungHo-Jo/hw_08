ed412cac33babdec2d15ad02666b863d15dd8813
owk4057 <owk4057@gmail.com>
2017-12-05 17:27:22 +0900

    Update terminal.h
    using dma

--- Libraries/terminal.h
+++ Libraries/terminal.h
@@ -1,31 +1,19 @@
-#ifndef __TERMINAL_H__
-#define __TERMINAL_H__
+#ifndef TERMINAL_H
+#define TERMINAL_H
 
-#include "init.h"
 #include "misc.h"
 #include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_usart.h"
-#include "stm32f10x_rcc.h"
+#include "stm32f10x_dma.h"
 
-#define TERMINAL_RX_BUFFER_SIZE 20
+#define TERMINAL_RX_BUFFER_SIZE (100) 
 
-typedef struct {
-	uint16_t TX;
-	uint16_t RX;
-	GPIO_TypeDef* GPIO;
-	char RX_Buffer[TERMINAL_RX_BUFFER_SIZE];
-	uint16_t RX_Counter;
-	uint8_t readyToSend;
-} Terminal_Struct;
+void Send_String_Usart2(int len);
+void Terminal_Struct_init(); 
+void Terminal_Rcc_init(); 
+void Terminal_Init(); 
+void USART1_IRQHandler(); 
 
-Terminal_Struct* Terminal_Struct_init();
-void Terminal_Rcc_init();
-void Terminal_init();
-void Terminal_IRQHandler();
-void USART1_IRQHandler(void);
-
-void Terminal_Run();
-
-
-#endif
+#endif TERMINAL_H
