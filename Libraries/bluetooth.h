29b8fc24a54ec91d010349a0d2f1830feeafba64
owk4057 <owk4057@gmail.com>
2017-12-08 00:11:52 +0900

    Update bluetooth.h

--- Libraries/bluetooth.h
+++ Libraries/bluetooth.h
@@ -8,12 +8,23 @@
 #include "stm32f10x_usart.h"
 #include "stm32f10x_dma.h"
 
-#define BT_RX_BUFFER_SIZE (1000) 
+#define BT_RX_BUFFER_SIZE (1000)
 
-void Send_String_Usart2(int len);
-void BT_Struct_init(); 
-void BT_Rcc_init(); 
-void BT_Init(); 
-void USART2_IRQHandler(); 
+typedef struct {
+	uint16_t TX;
+	uint16_t RX;
+	GPIO_TypeDef* GPIO;
+	char RX_Buffer[BT_RX_BUFFER_SIZE];
+	uint16_t RX_Counter;
+} BT_Struct;
 
-#endif 
+
+void BT_Struct_Init(void);
+void BT_Rcc_Init(void);
+void BT_Init(void);
+void USART2_IRQHandler(void);
+
+#endif
+
+
+
