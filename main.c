7b8fc2693e9004b6de5fe9b6df38bb4541598dfa
owk4057 <owk4057@gmail.com>
2017-12-08 00:13:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: 2017. 12. 7.
+ *      Author: owk
+ */
+
+
 #include "init.h"
 #include "bluetooth.h"
 #include "lcd.h"
 #include "Touch.h"
 #include "touchSensor.h"
 #include "terminal.h"
+#include "bluetooth.h"
 
-// flash load "./hw_team08/Debug/hw_team08.axf"
-// flash load "./hw_team08/flashClear.axf"
 
 int main(void) {
-	// set system and set clock
-	// RCC init part
 	defaultSystemInit();
-	Terminal_Rcc_init();
 
 	LCD_Init();
-	BT_RCC_init();
-	Terminal_Struct* ts = Terminal_Struct_init();
-
 	LCD_Clear(WHITE);
 	LCD_ShowNum(100, 200, 234, 5, WHITE, BLUE);
-
-	BT_init();
-	Terminal_init();
-
-	// gpio touch init와 init timer를 lcd init 보다 할 경우 lcd init이 제대로 이루어지지 않으며
-	// 계속 1의 값이 들어옴
 	GPIO_touch_init_all();
-	BT_Run();
-	Terminal_Run();
+
+	BT_Init();
+	Terminal_Init();
+
+
 	init_Timer2();
 
 	while (1) {
