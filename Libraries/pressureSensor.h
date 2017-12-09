fde8814409d59ff72a0fe6e4a4c7f04835dcba17
owk4057 <owk4057@gmail.com>
2017-12-09 14:00:30 +0900

    Update pressureSensor.h

--- Libraries/pressureSensor.h
+++ Libraries/pressureSensor.h
@@ -1,3 +1,6 @@
+#ifndef PRESSURE_SENSOR_H
+#define PRESSURE_SENSOR_H
+
 #include "stm32f10x_adc.h"
 #include "stm32f10x_dma.h"
 #include "stm32f10x_rcc.h"
@@ -5,7 +8,7 @@
 #include "misc.h"
 
 
-
-
 void GPIO_config(void);
 void adc_config(void);
+
+#endif
